cdbc8b3edb554597c8f551b088d16aa3029cd178
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-11 10:49:58 +0800

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Prakhar Trivedi on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
