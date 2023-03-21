823bbddabd24a70fafdf45bbbc76095252ad9920
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-21 19:51:09 +0800

    challenge for project 9 completed

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,102 +7,43 @@
 
 import SwiftUI
 
-struct Spirograph: Shape {
-    let innerRadius: Int
-    let outerRadius: Int
-    let distance: Int
-    let amount: Double
-    
-    // We need to prepare 3 values from the input data above
-    
-    // The first is the greatest common divisor (or GCD in mathematics) of the inner and outer radius. GCD is usually calculated using the Euclid's algorithm which looks a bit like this.
-    func gcd(_ a: Int, _ b: Int) -> Int {
-        var a = a
-        var b = b
-        
-        while b != 0 {
-            let temp = b
-            b = a % b
-            a = temp
-        }
-        
-        return a
-    }
-    
-    // The other two values are as follows: Difference between inner radius and outer radius, and the total number of steps that it takes to draw the roulette.
+struct Arrow: Shape {
     func path(in rect: CGRect) -> Path {
-        let divisor = gcd(innerRadius, outerRadius)
-        
-        // Create double copies for us to work with
-        let outerRadius = Double(self.outerRadius)
-        let innerRadius = Double(self.innerRadius)
-        let distance = Double(self.distance)
-        
-        let difference = innerRadius - outerRadius
-        let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
-        
-        // We can now draw our roulette by looping from 0 to our endPoint, placing points at specific x and y coordinates.
-        // Calclating those precise x and y coordinates (or theta) is where the real mathematics comes in. You can Google this equation for help because it is literally just the Swift version of it.
         var path = Path()
         
-        for theta in stride(from: 0, through: endPoint, by: 0.01) {
-            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
-            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
-            
-            x += rect.width / 2
-            y += rect.height / 2
-            
-            if theta == 0 {
-                path.move(to: CGPoint(x: x, y: y))
-            } else {
-                path.addLine(to: CGPoint(x: x, y: y))
-            }
-        }
+        // Draw the diagonal lines
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY / 2))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY / 2))
+        
+        // Draw central line branch
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        
+//        path.closeSubpath()
         
         return path
     }
 }
 
 struct ContentView: View {
-    // sensible defaults based on trial and error
-    @State private var innerRadius = 125.0
-    @State private var outerRadius = 75.0
-    @State private var distance = 25.0
-    @State private var amount = 1.0
-    @State private var hue = 0.6
+    @State var lineWidth: CGFloat = 10
     
     var body: some View {
-        VStack(spacing: 0) {
+        VStack {
             Spacer()
-            
-            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
-                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+            Arrow()
+                .stroke(.black, lineWidth: lineWidth)
                 .frame(width: 300, height: 300)
             
             Spacer()
             
-            Group {
-                Text("Inner radius: \(Int(innerRadius))")
-                Slider(value: $innerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Outer radius: \(Int(outerRadius))")
-                Slider(value: $outerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Distance: \(Int(distance))")
-                Slider(value: $distance, in: 1...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
-                Slider(value: $amount)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Colour")
-                Slider(value: $hue)
-                    .padding(.horizontal)
-            }
+            Slider(value: $lineWidth, in: 1...50)
+                .padding()
+            
+            Spacer()
         }
+        
     }
 }
 
